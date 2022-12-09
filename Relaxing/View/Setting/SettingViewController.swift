723236d5d0884b4825a0511adb6e308b83bd27b5
Ekko0701 <djekko0701@gmail.com>
2022-12-10 01:24:21 +0900

    Podfile commit

---
+++ Relaxing/View/Setting/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  Relaxing
+//
+//  Created by Ekko on 2022/12/09.
+//
+
+import Foundation
+import UIKit
+import Then
+import SnapKit
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemYellow
+        
+    }
+}
